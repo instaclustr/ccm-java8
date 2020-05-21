921d86b222382ba62e4032fed225ba8aa74624a1
Adam Zegelin <adam@zegelin.com>
2020-05-21 07:32:40 -0700

    Fixes issue where environment wasn't applied for nodetool and other C* tools. The fix is to now just globally set JAVA_HOME rather than through the append_to_server_env hook. Adds support for Debian & Arch Linux as a test.

--- setup.py
+++ setup.py
@@ -1,30 +1,32 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name='ccm_java8',
-    version='0.1',
-    packages=find_packages(),
+    version='1.0',
+    py_modules=['ccm_java8'],
 
     author="Adam Zegelin",
     author_email="adam@instaclustr.com",
 
-    description="CCM extension that starts nodes under Java 1.8",
+    description="CCM extension that starts Cassandra (and related tools) under Java 8",
     long_description=long_description,
     long_description_content_type="text/markdown",
 
     url="https://github.com/instaclustr/ccm-java8",
 
-    install_requires=['ccm'],
+    install_requires=['ccm', 'os-release'],
 
     entry_points={
-        'ccm_extension': ['java_home = ccm_java8.java_home:register_extensions']
+        'ccm_extension': ['java_home = ccm_java8:set_java_env']
     },
 
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Plugins',
         'Environment :: MacOS X',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
